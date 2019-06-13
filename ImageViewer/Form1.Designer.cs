226b7303ff3f1330ed9027dc26b43a9868ba4449
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-06-13 10:46:55 +0900

    #1 PictureBox, ImageList 등을 이용하여 사진첩 폼 디자인, 속성 설정

--- ImageViewer/Form1.Designer.cs
+++ ImageViewer/Form1.Designer.cs
@@ -29,12 +29,82 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.picImg = new System.Windows.Forms.PictureBox();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImg
+            // 
+            this.picImg.Location = new System.Drawing.Point(12, 12);
+            this.picImg.Name = "picImg";
+            this.picImg.Size = new System.Drawing.Size(256, 144);
+            this.picImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImg.TabIndex = 0;
+            this.picImg.TabStop = false;
+            // 
+            // lblNum
+            // 
+            this.lblNum.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblNum.Location = new System.Drawing.Point(104, 162);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(74, 61);
+            this.lblNum.TabIndex = 1;
+            this.lblNum.Text = "/";
+            this.lblNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgList
+            // 
+            this.imgList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imgList.ImageSize = new System.Drawing.Size(256, 144);
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(12, 162);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(86, 61);
+            this.btnPrev.TabIndex = 2;
+            this.btnPrev.Text = "이전";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(184, 162);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(86, 61);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "다음";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(282, 235);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.picImg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "사진첩";
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picImg;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
     }
 }
 
